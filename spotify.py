404c95f8b8aef789c8d05f7d863344843a2d87c2
Kayne Neigherbauer <kayne.n@gmail.com>
2018-06-30 20:27:59 -0500

    work on spotify api, search not there

--- spotify.py
+++ spotify.py
@@ -4,9 +4,8 @@ client_id = keys.spot_clientid
 client_secret = keys.spot_secret
 
 
-headers = {
-    'Content-Type': "application/x-www-form-urlencoded",
-    }
+#get token from spotify
+headers = {'Content-Type': "application/x-www-form-urlencoded"}
 params = {"grant_type":"client_credentials"}
 url = "https://accounts.spotify.com/api/token"
 
@@ -14,8 +13,21 @@ r = requests.request("POST", url, auth=(client_id, client_secret),headers=header
 
 
 if r.status_code == 200:
-    print(r.json)
-    toke= r.json.get["access_token"]
-
+    print(r.json())
+    response = r.json()
+    token = response.get("access_token")
+    artists = []
     with open(keys.path, "r") as f:
-        
+        for line in f:
+            artists.append(f.readline())
+    ## search artist
+    for artist in artists:
+        artist = artist.replace(" ","%20")
+        artist = artist.strip("\n")
+        artist = artist.strip('"')
+        print(artist)
+        url = "https://api.spotify.com/v1/search"
+        headers = {'Authorization': 'Bearer '+token}
+        pararms =  {'q': artist, 'type': 'artist', 'market':'from_token'}
+        r = requests.request("GET", url, headers= headers, params= params)
+        print(r.json())
